0e05ab58938dfb46b80d232c513334045bb4e4a2
DESKTOP-KT97LT3\LocNT <ngothanhloc90@gmail.com>
2022-09-25 08:55:52 +0700

    first commit

---
+++ inventory-service/src/main/java/com/demo/inventoryservice/model/Inventory.java
@@ -0,0 +1,24 @@
+package com.demo.inventoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_inventory")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+}
