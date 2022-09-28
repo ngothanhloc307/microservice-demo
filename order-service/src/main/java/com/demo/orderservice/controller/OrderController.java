668df5677eabb355bfe02bf626b64707a79ba5df
DESKTOP-KT97LT3\LocNT <ngothanhloc90@gmail.com>
2022-09-28 13:17:21 +0700

    keycloak authentication config

--- order-service/src/main/java/com/demo/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/demo/orderservice/controller/OrderController.java
@@ -2,21 +2,28 @@ package com.demo.orderservice.controller;
 
 import com.demo.orderservice.dto.OrderRequest;
 import com.demo.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/order")
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
+    private final OrderService orderService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return "Oops! some thing went wrong, please order some time!";
     }
 }
